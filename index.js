c9bc602f6dcc14060071574a1e7b3fc7a0c4ed34
bachvq <baveku@gmail.com>
2022-04-10 16:03:07 +0700

    fix mmkv tools

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ import App from './src/App'
 import Config from 'react-native-config'
 import { LogBox } from 'react-native'
 import { MMKV } from 'react-native-mmkv'
-const defaultStorage = new MMKV()
 // index.js
 if (__DEV__) {
 	require('react-native-performance-flipper-reporter').setupDefaultFlipperReporter()
-	// require("react-native-mmkv-flipper-plugin").initializeMMKVFlipper({ default: new MMKV() })
+	require("react-native-mmkv-flipper-plugin").initializeMMKVFlipper({ default: new MMKV() })
 }
 
 LogBox.ignoreLogs(['Remote debugger', 'NativeBase', 'RCTBridge'])
